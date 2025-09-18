0f203bfad8ff4bd77fa98f666b5aeb9ba454d597
illia_dp <illiachernysh.dp@gmail.com>
2025-09-18 12:32:13 +0300

    add sorting by rate and filter by category in stories/get route

--- src/controllers/stories.js
+++ src/controllers/stories.js
@@ -1,11 +1,16 @@
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { STORY_SORT_FIELDS } from '../constants/validation.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilters } from '../utils/parseFiltes.js';
 import { getStories } from '../services/stories.js';
 
 export const getStoriesController = async (req, res, next) => {
   const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query, STORY_SORT_FIELDS);
+  const filters = parseFilters(req.query);
 
   try {
-    const data = await getStories({ page, perPage });
+    const data = await getStories(page, perPage, sortBy, sortOrder, filters);
 
     res.json({
       status: 200,
