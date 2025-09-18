101e4004d918ef9bab848448feb5f1bca9331981
illia_dp <illiachernysh.dp@gmail.com>
2025-09-18 15:20:31 +0300

    add public get route stories/authors with pagination and sorting by name

--- src/routers/stories.js
+++ src/routers/stories.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { getStoriesController } from '../controllers/stories.js';
+import {
+  getStoriesAuthorsController,
+  getStoriesController,
+} from '../controllers/stories.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const storyRouter = Router();
 
 storyRouter.get('/', ctrlWrapper(getStoriesController));
+
+storyRouter.get('/authors', ctrlWrapper(getStoriesAuthorsController));
 
 export default storyRouter;
