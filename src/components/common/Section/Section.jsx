1783219a5c649baa34e197858dd77ecdb20377a9
ivannakm <ivadatalover@gmail.com>
2025-09-19 16:33:48 +0200

    add container/section

--- src/components/common/Section/Section.jsx
+++ src/components/common/Section/Section.jsx
@@ -7,15 +7,7 @@
  * @param {object} props - Властивості компонента
  * @param {React.ReactNode} props.children - Вміст секції
  * @param {string} [props.className] - Додаткові CSS класи
- * 
- !!!!!!!!* How to Use in JSX *!!!!!!!
-<Section className="pt-32 pt-md-64 pb-64 pb-lg-124">
-  ...
-</Section>
-Meaning:
-Mobile: padding-top: 32px, padding-bottom: 64px
-Tablet (768px+): padding-top: 64px
-Desktop (1440px+): padding-bottom: 124px
+ *
  */
 
 import css from './Section.module.css';
