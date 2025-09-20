80c742ad0895057c91e7b89226173f061f4fd0e7
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-09-20 07:07:42 -0600

    add mobile menu

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, NavLink, useLocation } from 'react-router-dom';
+import clsx from 'clsx';
+
 import Logo from '../Logo/logo.svg?react';
+import BurgerMenu from '../../../assets/icons/menu.svg?react';
+import BurgerClose from '../../../assets/icons/close.svg?react';
 
 import Container from '../Container/Container';
 import s from './Header.module.css';
@@ -10,53 +14,128 @@ const Header = () => {
   const location = useLocation();
   const isHome = location.pathname === '/';
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  // handler
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  //
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth < 1440);
+    };
+
+    checkIfMobile(); // Listen to when start
+    window.addEventListener('resize', checkIfMobile); //Listen to changes in size
+  }, []);
+
   // JSX
   return (
     <Container>
       <header className={s.header}>
         <NavLink to="/">
           <Logo
+            className={s.logo}
             style={{
-              fill: isHome
-                ? 'var(--color-white)'
-                : 'var(--color-scheme-1-text)',
+              fill:
+                isHome && !isMenuOpen
+                  ? 'var(--color-white)'
+                  : 'var(--color-scheme-1-text)',
             }}
           />
         </NavLink>
-        <nav className={s.nav}>
-          <ul>
-            <li>
-              <Link to="/">Головна</Link>
-            </li>
-            <li>
-              <Link to="/stories">Історії</Link>
-            </li>
-            <li>
-              <Link to="/travellers">Мандрівники</Link>
-            </li>
-            <li>
-              <AppButton
-                href="/auth/login"
-                variant="grey"
-                width="58px"
-                height="35px"
-              >
-                Вхід
-              </AppButton>
-            </li>
-            <li>
-              <AppButton
-                href="/auth/register"
-                variant="dark"
-                width="113px"
-                height="35px"
-              >
-                Реєстрація
-              </AppButton>
-            </li>
-          </ul>
-        </nav>
+
+        {/* Desktop */}
+        {!isMobile && (
+          <nav className={s.nav}>
+            <ul>
+              <li>
+                <Link to="/">Головна</Link>
+              </li>
+              <li>
+                <Link to="/stories">Історії</Link>
+              </li>
+              <li>
+                <Link to="/travellers">Мандрівники</Link>
+              </li>
+              <li>
+                <AppButton
+                  href="/auth/login"
+                  variant="grey"
+                  width="58px"
+                  height="35px"
+                >
+                  Вхід
+                </AppButton>
+              </li>
+              <li>
+                <AppButton
+                  href="/auth/register"
+                  variant="dark"
+                  width="113px"
+                  height="35px"
+                >
+                  Реєстрація
+                </AppButton>
+              </li>
+            </ul>
+          </nav>
+        )}
+
+        {isMobile && (
+          <AppButton variant="grey" onClick={toggleMenu}>
+            {isMenuOpen ? <BurgerClose /> : <BurgerMenu />}
+          </AppButton>
+        )}
       </header>
+
+      {/* Mobile */}
+      {isMobile && isMenuOpen && (
+        <div className={clsx(s.overlay, isMenuOpen && s['isOpen'])}>
+          <nav className={s.nav}>
+            <ul>
+              <li>
+                <Link to="/" onClick={toggleMenu}>
+                  Головна
+                </Link>
+              </li>
+              <li>
+                <Link to="/stories" onClick={toggleMenu}>
+                  Історії
+                </Link>
+              </li>
+              <li>
+                <Link to="/travellers" onClick={toggleMenu}>
+                  Мандрівники
+                </Link>
+              </li>
+              <li>
+                <AppButton
+                  href="/auth/login"
+                  variant="grey"
+                  width="355px"
+                  height="35px"
+                >
+                  Вхід
+                </AppButton>
+              </li>
+              <li>
+                <AppButton
+                  href="/auth/register"
+                  variant="blue"
+                  width="355px"
+                  height="35px"
+                >
+                  Реєстрація
+                </AppButton>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      )}
     </Container>
   );
 };
