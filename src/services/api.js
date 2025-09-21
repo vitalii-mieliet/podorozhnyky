d3234dec55478fb1623c570304bd200475d1d7ec
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-09-21 16:25:42 +0300

    refactor(api): add BASE_API_URL constant into api servise.

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
+import { BASE_API_URL } from '../constants/api';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: BASE_API_URL,
 });
