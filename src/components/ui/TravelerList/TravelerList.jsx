43589005b836735b9e96b4ec8273338da76e0563
FounDy <vkadgcundgd@gmail.com>
2025-09-21 19:07:33 +0200

    ListGrif

--- src/components/ui/TravelerList/TravelerList.jsx
+++ src/components/ui/TravelerList/TravelerList.jsx
@@ -3,16 +3,17 @@ import css from './TravelerList.module.css';
 
 function TravelerList({ travelers = [] }) {
   if (!Array.isArray(travelers)) {
-    console.warn('TravelerList expects an array, but got:', travelers);
     return null;
   }
 
   return (
-    <div className={css.cardsGrid}>
-      {travelers.map((t, idx) => (
-        <TravelerCard key={idx} userData={t} />
+    <ul className={css.cardsGrid}>
+      {travelers.map((t) => (
+        <li key={t._id} className={css.cardItem}>
+          <TravelerCard userData={t} />
+        </li>
       ))}
-    </div>
+    </ul>
   );
 }
 
