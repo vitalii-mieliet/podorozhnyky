9957ca6bc4b6bb5ccf1a32a91b13737e0349648d
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-09-23 15:13:30 +0300

    feat(api): add withCredentials and token store
    - enabled `withCredentials: true` in axios instance;
    - added `tokenStore.js` for managing Authorization header (set/clear access token).

---
+++ src/services/api/tokenStore.js
@@ -0,0 +1,13 @@
+import { api } from '.';
+
+export const setAccessToken = (token) => {
+  if (token) {
+    api.defaults.headers.common.Authorization = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common.Authorization;
+  }
+};
+
+export const clearAccessToken = () => {
+  delete api.defaults.headers.common['Authorization'];
+};
