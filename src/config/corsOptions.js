347e0dc2976b5b3cc85772bac751c10a9bb07cd9
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-09-23 17:06:15 +0300

    feat(config): add CORS configuration with credentials support - add config/corsOptions.js with allowed origins and headers - enable credentials: true for cookies and auth headers - apply CORS middleware in app.js using the new config

---
+++ src/config/corsOptions.js
@@ -0,0 +1,18 @@
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://plantains-app-dev.vercel.app', // dev
+  'https://plantains-app-client.vercel.app', //product
+];
+
+export const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
