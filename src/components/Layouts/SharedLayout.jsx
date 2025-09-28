5475d553a1acca3f595a64c26a9f8c6fd2507704
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-09-28 22:27:45 +0300

    feat(layout): apply flex layout to SharedLayout
    - wrapped layout content in flex container with min-height: 100vh
    - added flex: 1 to main to stretch and keep footer at the bottom

--- src/components/Layouts/SharedLayout.jsx
+++ src/components/Layouts/SharedLayout.jsx
@@ -1,18 +1,19 @@
+import { Outlet, useLocation } from 'react-router-dom';
 import Footer from '../common/Footer/Footer';
 import Header from '../common/Header/Header';
-import { Outlet, useLocation } from 'react-router-dom';
+import styles from './SharedLayout.module.css';
 
 const SharedLayout = () => {
   const location = useLocation();
   const hidenFooterRoutes = ['/auth/login', '/auth/register'];
   return (
-    <>
+    <div className={styles.layout}>
       <Header />
-      <main>
+      <main className={styles.main}>
         <Outlet />
       </main>
       {!hidenFooterRoutes.includes(location.pathname) && <Footer />}
-    </>
+    </div>
   );
 };
 
