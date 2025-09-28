fe25a0b4d57fd2faa442a4cacffd2d1bc780b8b7
Danya76131 <danylo.galetskiy@gmail.com>
2025-09-28 12:55:54 +0200

    fix authPage

--- src/components/Layouts/SharedLayout.jsx
+++ src/components/Layouts/SharedLayout.jsx
@@ -4,14 +4,14 @@ import { Outlet, useLocation } from 'react-router-dom';
 
 const SharedLayout = () => {
   const location = useLocation();
-  const hideFooterRoutes = ['/auth/login', '/auth/register'];
+  const hidenFooterRoutes = ['/auth/login', '/auth/register'];
   return (
     <>
       <Header />
       <main>
         <Outlet />
       </main>
-      {!hideFooterRoutes.includes(location.pathname) && <Footer />}
+      {!hidenFooterRoutes.includes(location.pathname) && <Footer />}
     </>
   );
 };
