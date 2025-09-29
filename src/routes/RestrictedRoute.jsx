1ecb845a06041d3bef6f9a332662f5b6131e4d49
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-09-29 14:03:14 +0300

    fix(routes): add Loader to RestrictedRoute component

--- src/routes/RestrictedRoute.jsx
+++ src/routes/RestrictedRoute.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { selectAuthState } from '../redux/auth/selectors';
+import Loader from '../components/common/Loader/Loader';
 
 const RestrictedRoute = ({ children }) => {
   const { isLoggedIn, isInitialized } = useSelector(selectAuthState);
